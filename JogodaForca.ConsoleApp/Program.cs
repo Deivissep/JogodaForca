b756e376df0830440580c51dc8c3e96461411943
Deivissep <daavi2223@gmail.com>
2025-03-25 19:27:19 -0300

    V01 - Estrutura basica e entrada do usuário

--- JogodaForca.ConsoleApp/Program.cs
+++ JogodaForca.ConsoleApp/Program.cs
@@ -2,9 +2,25 @@
 {
     internal class Program
     {
+
+        //V01 - Estrutura basica e entrada do usuário
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Jogo da Forca");
+                Console.WriteLine("---------------------------------");
+
+                Console.Write("Digite uma letra: ");
+                char chute = Console.ReadLine()[0]; // pega apenas um caracter que usuário digita
+
+                Console.WriteLine("Você digitou a letra: " + chute);
+                Console.ReadLine();
+
+            }
+
         }
     }
 }
