749b67c346b3ffc153dd9115b5ef216af98186d2
Deivissep <daavi2223@gmail.com>
2025-03-25 19:49:45 -0300

    Implementada funcionalidade de exibição da dica da palavra

--- JogodaForca.ConsoleApp/Program.cs
+++ JogodaForca.ConsoleApp/Program.cs
@@ -3,14 +3,29 @@
     internal class Program
     {
 
-        //V01 - Estrutura basica e entrada do usuário
+        //V02 - Exibir palavra ocula com traços
         static void Main(string[] args)
         {
             while (true)
             {
+
+                string palavraSecreta = "MELANCIA";
+
+                char[] letrasEncontradas = new char[palavraSecreta.Length];
+
+                for (int caractere = 0; caractere < letrasEncontradas.Length; caractere++)
+                {
+                    //acessar o arry no indice 0
+                    letrasEncontradas[caractere] = '_';
+                }
+
+                string dicaDaPalavra = string.Join(" ", letrasEncontradas);
+
                 Console.Clear();
                 Console.WriteLine("---------------------------------");
                 Console.WriteLine("Jogo da Forca");
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Palavra secreta: " + dicaDaPalavra);
                 Console.WriteLine("---------------------------------");
 
                 Console.Write("Digite uma letra: ");
